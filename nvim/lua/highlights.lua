de5ce828899217c3a2fd0e323d92d8c8dd65664f
Sabah <sabah1994786@gmail.com>
2022-08-02 22:59:45 +0100

    changed visually highlighted text colour

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -1,3 +1,6 @@
 vim.highlight.create("NormalFloat", { guifg = "#ebdbb2", guibg = "#282828" })
 -- FloatBorder
-vim.highlight.create("FloatBorder", { guifg = "#5F5F5F", gui = "bold" })
+vim.highlight.create("FloatBorder", { guifg = "#363630", gui = "bold" })
+
+-- visually highlighted text colour
+vim.highlight.create("Visual", { guibg = "#4a2424" })
