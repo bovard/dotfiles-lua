510feec6db54c9e3bb913c40dc228dabd2833de4
Sabah <sabah1994786@gmail.com>
2022-08-03 11:28:06 +0100

    Added highlight group for word under cursor

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -4,3 +4,6 @@ vim.highlight.create("FloatBorder", { guifg = "#363630", gui = "bold" })
 
 -- visually highlighted text colour
 vim.highlight.create("Visual", { guibg = "#4a2424" })
+
+-- for word under cursor | "PeterRincker/vim-searchlight"
+vim.highlight.create("Searchlight", { guifg = "#202020", guibg = "#cc241d" })
